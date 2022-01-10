ec5891a49fc0ee240686dfcdf744c138a32f4fba
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2022-01-10 15:36:21 +0300

    Refactoring for my bro !

--- OOPAssignment/Base/Surface.cs
+++ OOPAssignment/Base/Surface.cs
@@ -9,7 +9,8 @@ namespace OOPAssignment.Base
     {
         public long Height { get; set; }
         public long Width { get; set; }
-        private List<CarInfo> ObservableCars = new List<CarInfo>();
+
+        private List<CarInfo> ObservableCars = new();
 
         public Surface(long width, long height)
         {
@@ -19,58 +20,40 @@ namespace OOPAssignment.Base
 
         public bool IsCoordinatesInBounds(Coordinates coordinates)
         {
-
             //Is the car Between X and Y Coordinates...
-            bool coordinate = false;
-            if ((coordinates.X >= 0 && coordinates.X <= Width) && (coordinates.Y >= 0 && coordinates.Y <= Height))
-            {
-                coordinate = true;
-            }
-            return coordinate;
+            return (coordinates.X >= 0 && coordinates.X <= Width) && (coordinates.Y >= 0 && coordinates.Y <= Height);
         }
         public bool IsCoordinatesEmpty(Coordinates coordinates)
         {
-
             //checking the car crash if the x or y coordinate exist any can it should be jump to  throw exception
             var IsCar = ObservableCars.Find(x => x.Coordinates.X == coordinates.X && x.Coordinates.X == coordinates.Y);
-            if (IsCar is not null)
-            {
-                return false;
-            }
-            return true;
 
-
+            return IsCar != null;
         }
         public void Update(CarInfo carInfo)
         {
             var carId = ObservableCars.Find(x => x.CarId == carInfo.CarId);
             Coordinates coordinates = carInfo.Coordinates;
-            if (IsCoordinatesInBounds(coordinates) == false)
-            {
+            if (IsCoordinatesInBounds(coordinates))
                 throw new Exception("car is out of the line");
-            }
-            if (IsCoordinatesInBounds(coordinates) == true)
+
+
+
+            if (IsCoordinatesInBounds(coordinates))
             {
                 var car = ObservableCars.Find(x => x.CarId != carInfo.CarId && x.Coordinates.X == carInfo.Coordinates.X && x.Coordinates.Y == carInfo.Coordinates.Y);
                 if (car is not null)
-                {
                     throw new Exception("car crashed");
-                }
                 else if (carId is not null)
-                {
                     carId = carInfo;
-                }
                 else
-                {
                     //add new car if there is not any car in the coordinates
                     ObservableCars.Add(carInfo);
-                }
-
             }
         }
         public List<CarInfo> GetObservables()
         {
-            List<CarInfo> carInfo = new List<CarInfo>();
+            List<CarInfo> carInfo = new();
             carInfo.AddRange(ObservableCars);
             return carInfo;
         }
